1b3301437f6bfb1b501080a904caefd811b6f731
ahsan-qliq <uiuxdev1@ezdigital.ae>
2026-03-01 21:34:23 +0400

    oading component added

---
+++ frontend/src/components/shared/Loading/Loading.tsx
@@ -0,0 +1,25 @@
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="skeleton skeleton-large" />
+
+      <div className="loading-grid">
+        <div className="skeleton skeleton-medium" />
+        <div className="skeleton skeleton-medium" />
+      </div>
+
+      <div className="skeleton skeleton-xlarge" />
+      <div className="skeleton skeleton-large" />
+
+      <div className="loading-stack">
+        <div className="skeleton skeleton-stack-item" />
+        <div className="skeleton skeleton-stack-item" />
+        <div className="skeleton skeleton-stack-item" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
