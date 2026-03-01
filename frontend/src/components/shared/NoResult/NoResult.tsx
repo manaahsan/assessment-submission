501cc22b5c2551fc2ef056449a9e4b535a68fcb4
ahsan-qliq <uiuxdev1@ezdigital.ae>
2026-03-01 21:34:02 +0400

    result not found component added

---
+++ frontend/src/components/shared/NoResult/NoResult.tsx
@@ -0,0 +1,18 @@
+import { ClipboardList } from "lucide-react";
+import './NoResult.css'
+
+const NoResults = () => {
+  return (
+    <div className="no-results-card">
+      <div className="no-results-icon">
+        <ClipboardList className="icon" />
+      </div>
+      <h3 className="no-results-title">No Results Loaded</h3>
+      <p className="no-results-text">
+        Enter an Assessment Instance ID above to load and visualize your results.
+      </p>
+    </div>
+  );
+};
+
+export default NoResults;
